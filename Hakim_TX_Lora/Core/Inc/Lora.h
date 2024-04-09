09fc685acfa0f50c44ee2033462665eeb61516fc
Godo <139667276+GodoSanchezHeredia@users.noreply.github.com>
2024-04-09 05:15:48 -0500

    V1
    Modo Normal Transparent , No RTOS

---
+++ Hakim_TX_Lora/Core/Inc/Lora.h
@@ -0,0 +1,16 @@
+/*
+ * Lora.h
+ *
+ *  Created on: Apr 9, 2024
+ *      Author: DELL
+ */
+
+#ifndef INC_LORA_H_
+#define INC_LORA_H_
+
+
+void Lora_Init(void);
+void Lora_Tx(void);
+
+
+#endif /* INC_LORA_H_ */
---
+++ Hakim_TX_Lora/Core/Src/Lora.c
@@ -0,0 +1,42 @@
+/*
+ * Lora.c
+ *
+ *  Created on: Apr 9, 2024
+ *      Author: DELL
+ */
+
+
+#include "Lora.h"
+#include "main.h"
+#include "string.h"
+
+extern ADC_HandleTypeDef hadc1;
+extern UART_HandleTypeDef huart5;
+
+void Lora_Init(void){
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(M0_GPIO_Port, M0_Pin,1);
+	HAL_GPIO_WritePin(M1_GPIO_Port, M1_Pin,1);
+	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin,1);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(M0_GPIO_Port, M0_Pin,0);
+	HAL_GPIO_WritePin(M1_GPIO_Port, M1_Pin,0);
+	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin,0);
+	HAL_Delay(200);
+
+}
+uint16_t ADCValue = 0;
+uint8_t dato_tx[25] = "";
+void Lora_Tx(void){
+
+	HAL_ADC_Start(&hadc1);
+	if (HAL_ADC_PollForConversion(&hadc1, 1000000) == HAL_OK){
+	ADCValue = HAL_ADC_GetValue(&hadc1);
+	}
+	HAL_ADC_Stop(&hadc1);
+
+	sprintf(dato_tx,"%u*",ADCValue);
+
+HAL_UART_Transmit(&huart5, dato_tx, strlen(dato_tx),8000);
+
+}
